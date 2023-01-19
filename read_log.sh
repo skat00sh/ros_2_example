c86c3cd21f5cdb4f366899b2f34e6b263d37ea73
Devendra <skatoosh@pop-os.localdomain>
2023-01-19 13:07:26 +0100

    create a swap file for diff comparison

--- read_log.sh
+++ read_log.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-diff test.txt test2.txt
-cp test.txt test2.txt
+touch .cpu_load.log
+diff cpu_load.log .cpu_load.log
+cp cpu_load.log .cpu_load.log
 
 #  while sleep 1; do tput clear; less +F test.txt; done
