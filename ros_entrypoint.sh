1730f6db3ff304d90804901136daedc46d37c662
Devendra <skatoosh@pop-os.localdomain>
2023-01-19 13:05:41 +0100

    Add colcon build instruction to ros_entrypoint script

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
 #script to run with a host network
 # export ROS_MASTER_URI=http://$(hostname --ip-address):11311
 # export ROS_HOSTNAME=$(hostname --ip-address)
 # setup ros environment
-echo "Hello from container"
+
+echo "Starting ROS Node to publish CPU Utilization..."
+source /opt/ros/foxy/setup.bash
+colcon build --packages-select cpp_pubsub
 source install/setup.bash 
 ros2 run cpp_pubsub talker
